8f0b7f95226059fddd8db556dbf5808af7c6b159
Meggan M Green <github@meggangreen.com>
2018-04-02 02:57:55 +0000

    @wip having some real trouble reading in bytes

--- proto.py
+++ proto.py
@@ -1,1 +1,17 @@
 """ Parse binary log file. """
+
+def _get_bin_to_utf_lines(file_path):
+    """ Return array of lines in file. """
+
+    with open(file_path, 'rb') as file_name:
+        lines = file_name.readlines().decode()
+
+    # return lines
+    for line in lines:
+        print(line)
+
+
+def _convert_to_unicode(byte_s, utf_spec):
+    """ Returns unicode string at specified encoding. """
+
+    return byte_s.decode('utf-')
